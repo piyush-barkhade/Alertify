121562aa49204fff899b77d4e336a9bab99322d4
piyush-barkhade <166143725+piyush-barkhade@users.noreply.github.com>
2025-03-31 15:57:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://alertify-0ohb.onrender.com" });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
